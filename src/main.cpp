97a6d99f02c1611dc20aee14bc5b7b80e94851b8
tseng <s07151@ms1.hinet.net>
2022-11-28 14:12:15 +0800

    init commit

---
+++ src/main.cpp
@@ -0,0 +1,21 @@
+#include <Arduino.h>
+#include <BluetoothSerial.h>
+BluetoothSerial BT;
+void setup() {
+  Serial.begin(115200);
+  BT.begin("tsengBT");   //藍芽顯示名稱,請修改為自訂名稱
+}
+
+void loop() {
+  
+  while (Serial.available()) {   //檢查序列內是否有資料
+    String Sdata = Serial.readString();   //讀取序列資料
+    BT.println(Sdata);   //傳輸給藍芽
+  }
+  
+  while (BT.available()) {   //檢查藍芽內是否有資料
+    String BTdata = BT.readString();    //讀取藍芽資料
+    Serial.println(BTdata);   //顯示在序列視窗
+  }
+  delay(1);
+}
